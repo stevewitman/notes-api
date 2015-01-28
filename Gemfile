65cc17ff914b3a48f08e939c3b92f9247ea4a571
Steve Witman <stevewitman@gmail.com>
2015-01-28 11:31:53 -0700

    directed storage to rails api

--- Gemfile
+++ Gemfile
@@ -13,6 +13,10 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'rack-cors', :require => 'rack/cors'
+
+gem 'active_model_serializers', '~> 0.9.1'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,46 @@
 class NotesController < ApplicationController
+
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+
   def index
     @notes = Note.all
     render json: @notes
   end
+
+  def show
+    render json: @note
+  end
+
+  def edit
+    render json: @note
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      render json: @note
+    end
+  end
+
+  def update
+    if @note.update(note_params)
+      render json: @note
+    end
+  end
+
+  def destroy
+    @note.destroy
+    render json: @note
+  end
+
+  private # ******************************************************************
+
+    def set_note
+      @note = Note.find(params[:id])
+    end
+
+    def note_params
+      params.require(:note).permit(:title, :body)
+    end
+
 end
